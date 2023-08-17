682f182170c1cc7f43d04b446580d29c22d5c218
VladimirSazanakov <sazanakov@inbox.ru>
2023-08-18 00:55:10 +0700

    3.3.6 beta

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,17 +7,33 @@ import Filter from '../Filter';
 import Tabs from '../Tabs';
 import Ticked from '../Ticket';
 
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap');
+</style>
+
 const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <Filter />
-      <Tabs />
-      <Ticked />
-      content
-      footer
+      <header className='app-header'>
+        <img src={logo} className="app-logo" alt="logo" />
+      </header>
+      <main className='app-main'>
+        <section className='app-filter-section'>
+          <Filter />
+
+        </section>
+        <section className='app-result-section'>
+          <Tabs />
+          <Ticked />
+          <Ticked />
+          <Ticked />
+          <Ticked />
+          <Ticked />
+
+        </section>
+      </main>
     </div>
   );
 }
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -16,17 +16,38 @@ export default function Tabs(props: any) {
     setValue4(value);
   };
 
+  const tabButtonStyle = {
+    height: 50,
+    width: 168,
+    fontSize: '12px',
+    fontFamily: 'Open Sans',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    lineHeight: '20px',
+    letterSpacing: '0.5px',
+    verticalAlign: 'baseline',
+
+  }
+
   return (
-    <Radio.Group
-      className="Tabs"
-      style={{ width: 504, height: 50, fontSize: 14 }}
-      size="large"
-      options={optionsWithDisabled}
-      onChange={onChange4}
-      value={value4}
-      optionType="button"
-      buttonStyle="solid"
-    />
+
+    <Radio.Group className="tabs" defaultValue="a" buttonStyle="solid">
+      <Radio.Button value="a" style={tabButtonStyle}>Hangzhou</Radio.Button>
+      <Radio.Button value="b" style={tabButtonStyle}>Shanghai</Radio.Button>
+      <Radio.Button value="c" style={tabButtonStyle}>Beijing</Radio.Button>
+    </Radio.Group>
+
+
+    // <Radio.Group
+    //   className="Tabs"
+    //   style={{ width: 504, height: 50, fontSize: 14 }}
+    //   size="middle"
+    //   options={optionsWithDisabled}
+    //   onChange={onChange4}
+    //   value={value4}
+    //   optionType="button"
+    //   buttonStyle="solid"
+    // />
 
   )
 }
